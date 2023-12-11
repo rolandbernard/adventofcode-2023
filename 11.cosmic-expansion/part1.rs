6ca5494f6c2bad10f9ba23780b37bc948693011a
Roland Bernard <rolli.bernard@gmail.com>
2023-12-11 21:02:34 +0100

    Solved day 11 puzzles

---
+++ 11.cosmic-expansion/part1.rs
@@ -0,0 +1,25 @@
+use std::collections::BTreeSet;
+
+fn main() {
+    let mut filled_rows = BTreeSet::new();
+    let mut filled_cols = BTreeSet::new();
+    let mut galaxies = Vec::new();
+    for (i, line) in std::io::stdin().lines().enumerate() {
+        for (j, char) in line.unwrap().chars().enumerate() {
+            if char == '#' {
+                filled_rows.insert(i);
+                filled_cols.insert(j);
+                galaxies.push((i, j));
+            }
+        }
+    }
+    let mut sum = 0;
+    for (k, &(i0, j0)) in galaxies.iter().enumerate() {
+        for &(i1, j1) in galaxies.iter().skip(k + 1) {
+            let di = 2 * i1.abs_diff(i0) - filled_rows.range(i0.min(i1)..i0.max(i1)).count();
+            let dj = 2 * j1.abs_diff(j0) - filled_cols.range(j0.min(j1)..j0.max(j1)).count();
+            sum += di + dj;
+        }
+    }
+    println!("Result: {}", sum);
+}
